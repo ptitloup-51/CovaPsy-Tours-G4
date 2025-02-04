00c60d178f532b49c04d79b223cc07c1d2b1ec19
battd <batt.depaul@gmail.com>
2025-02-04 14:30:35 +0100

    Suppression Lidar non fonctionnel

--- VoitureAutonome/obj/Debug/net8.0/VoitureAutonome.AssemblyInfo.cs
+++ VoitureAutonome/obj/Debug/net8.0/VoitureAutonome.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoitureAutonome")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e4692bee2a9ba1e0fb75c0411830551d856b4f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b24d7486741c6a0755f613c877218e8291fd057")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoitureAutonome")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoitureAutonome")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
