82e352453b5691d1e74eb6cd7b3dc708b7631312
timot <timothee.jaffres@gmail.com>
2025-02-04 09:34:14 +0100

    class direction

--- VoitureAutonome/obj/Debug/net8.0/VoitureAutonome.AssemblyInfo.cs
+++ VoitureAutonome/obj/Debug/net8.0/VoitureAutonome.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoitureAutonome")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54cc05cbbb5898054f3137b7293113875ae32409")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37a1d0761fae28bbc54ad4e42dc77da2d517baac")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoitureAutonome")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoitureAutonome")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
