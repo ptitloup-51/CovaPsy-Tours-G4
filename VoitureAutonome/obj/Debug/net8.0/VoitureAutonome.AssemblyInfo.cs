294a11ebdd12214371c8907c801fe8ffe8c4ac21
battd <batt.depaul@gmail.com>
2025-02-27 10:43:47 +0100

    Test récupération valeur vitesse

--- VoitureAutonome/obj/Debug/net8.0/VoitureAutonome.AssemblyInfo.cs
+++ VoitureAutonome/obj/Debug/net8.0/VoitureAutonome.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoitureAutonome")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8d9d21540f5807b5e3bf0a63b264ae1fed4d131")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50eda6c52ac2001acfd609f1a55435e965aad63c")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoitureAutonome")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoitureAutonome")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
