a748d2f3ecaa4cc3471601a3a758e98f44d54577
Loup Lavabre <32581655+ptitloup-51@users.noreply.github.com>
2025-03-05 09:38:30 +0100

    ??

--- VoitureAutonome/obj/Debug/net8.0/VoitureAutonome.AssemblyInfo.cs
+++ VoitureAutonome/obj/Debug/net8.0/VoitureAutonome.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoitureAutonome")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+982407f67ce3ddf2ae5820d022669985145b25ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7dbb315b001bb182af6d71a8f4c4743aaaf5062")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoitureAutonome")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoitureAutonome")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
